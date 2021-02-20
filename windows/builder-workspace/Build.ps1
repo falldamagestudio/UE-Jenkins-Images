e7c47f2a69b2b56defde0394cc3ba08f273f5827
Mikael Kalms <mikael@kalms.org>
2021-02-20 23:50:23 +0100

    Added some progress printouts

--- windows/builder-workspace/Build.ps1
+++ windows/builder-workspace/Build.ps1
@@ -4,15 +4,29 @@ param (
 	[Parameter(Mandatory=$true)][string]$ImageTag
 )
 
+Write-Host "Resizing C: partition to use all available disk space..."
+
 . "${PSScriptRoot}\Resize-PartitionToMaxSize.ps1"
 Resize-PartitionToMaxSize -DriveLetter C
 
+Write-Host "Installing test dependencies..."
+
 & "${PSScriptRoot}\InstallTestDependencies.ps1"
+
+Write-Host "Running tests for Powershell scripts..."
 
 & "${PSScriptRoot}\RunTests.ps1"
 
+Write-Host "Setting up Docker authentication..."
+
 & "${PSScriptRoot}\SetupDockerRegistryAuthentication.ps1" -GceRegion ${GceRegion}
+
+Write-Host "Building image..."
 
 & "${PSScriptRoot}\BuildImage.ps1" -ImageName ${ImageName} -ImageTag ${ImageTag}
 
+Write-Host "Pushing image to registry..."
+
 & "${PSScriptRoot}\PushImage.ps1" -ImageName ${ImageName} -ImageTag ${ImageTag}
+
+Write-Host "Done."
