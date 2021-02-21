798d606bd1b3d6ad81d08d81c45c6e8ad6504467
Mikael Kalms <mikael@kalms.org>
2021-02-21 17:46:43 +0100

    Removed some debug prints

--- windows/builder-workspace/InstallTestDependencies.ps1
+++ windows/builder-workspace/InstallTestDependencies.ps1
@@ -1,13 +1,9 @@
 # Install Pester (latest version, v5.0.0 or newer)
-Install-Module -Name Pester -Force -SkipPublisherCheck -MinimumVersion 5.0.0 -ErrorAction Stop -Verbose | Out-Host
+Install-Module -Name Pester -Force -SkipPublisherCheck -MinimumVersion 5.0.0 -ErrorAction Stop
 Get-Module -Name Pester | Remove-Module
-Import-Module -Name Pester -MinimumVersion 5.0.0 -ErrorAction Stop -Verbose | Out-Host
-Get-Module -Name Pester -Verbose | Out-Host
-Get-Command -Module pester -Verbose | Select-Object -Property name, version -First 3 | Out-Host
+Import-Module -Name Pester -MinimumVersion 5.0.0 -ErrorAction Stop
 
 # Install https://github.com/nohwnd/Assert
 Install-Module -Name Assert -Force -MinimumVersion 0.9.5 -ErrorAction Stop
 Get-Module -Name Assert | Remove-Module
 Import-Module Assert -ErrorAction Stop
-
-Get-Module -Name Assert -Verbose | Out-Host
