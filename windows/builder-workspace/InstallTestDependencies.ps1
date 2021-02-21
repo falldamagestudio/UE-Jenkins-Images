cd3e2d821250129983261b08458d039996e878cf
Mikael Kalms <mikael@kalms.org>
2021-02-21 10:53:38 +0100

    Added more debug logs

--- windows/builder-workspace/InstallTestDependencies.ps1
+++ windows/builder-workspace/InstallTestDependencies.ps1
@@ -1,16 +1,18 @@
 # Install Pester (latest version, v5.0.0 or newer)
 Write-Host "PSVersion:"
 $PSVersionTable.PSVersion | Out-Host
-Find-Module -Name Pester | Out-Host
-Get-Module -Name Pester | Out-Host
+Find-Module -Name Pester -Verbose | Out-Host
+Get-Module -Name Pester -Verbose | Out-Host
 Get-Command -Module pester | Select-Object -Property name, version -First 3 | Out-Host
-Install-Module -Name Pester -Force -SkipPublisherCheck -ErrorAction Stop
-Get-Module -Name Pester | Out-Host
-Get-Command -Module pester | Select-Object -Property name, version -First 3 | Out-Host
-Import-Module Pester -ErrorAction Stop
-Get-Module -Name Pester | Out-Host
-Get-Command -Module pester | Select-Object -Property name, version -First 3 | Out-Host
+Install-Module -Name Pester -Force -SkipPublisherCheck -ErrorAction Stop -Verbose | Out-Host
+Get-Module -Name Pester -Verbose | Out-Host
+Get-Command -Module pester -Verbose | Select-Object -Property name, version -First 3 | Out-Host
+Import-Module Pester -ErrorAction Stop -Verbose | Out-Host
+Get-Module -Name Pester -Verbose | Out-Host
+Get-Command -Module pester -Verbose | Select-Object -Property name, version -First 3 | Out-Host
 
 # Install https://github.com/nohwnd/Assert
 Install-Module -Name Assert -Force -ErrorAction Stop
 Import-Module Assert -ErrorAction Stop
+
+Get-Module -Name Assert -Verbose | Out-Host
