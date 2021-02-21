a28a602ec77362bc5a404c2115f3a00dbbe336be
Mikael Kalms <mikael@kalms.org>
2021-02-21 17:08:30 +0100

    Remove old versions of modules before installing new ones

--- windows/builder-workspace/InstallTestDependencies.ps1
+++ windows/builder-workspace/InstallTestDependencies.ps1
@@ -1,18 +1,13 @@
 # Install Pester (latest version, v5.0.0 or newer)
-Write-Host "PSVersion:"
-$PSVersionTable.PSVersion | Out-Host
-Find-Module -Name Pester -Verbose | Out-Host
-Get-Module -Name Pester -Verbose | Out-Host
-Get-Command -Module pester | Select-Object -Property name, version -First 3 | Out-Host
-Install-Module -Name Pester -Force -SkipPublisherCheck -ErrorAction Stop -Verbose | Out-Host
-Get-Module -Name Pester -Verbose | Out-Host
-Get-Command -Module pester -Verbose | Select-Object -Property name, version -First 3 | Out-Host
-Import-Module -Name Pester -RequiredVersion 5.1.1 -ErrorAction Stop -Verbose | Out-Host
+Install-Module -Name Pester --Force -SkipPublisherCheck -MinimumVersion 5.0.0 -ErrorAction Stop -Verbose | Out-Host
+Get-Module -Name Pester | Remove-Module
+Import-Module -Name Pester -MinimumVersion 5.0.0 -ErrorAction Stop -Verbose | Out-Host
 Get-Module -Name Pester -Verbose | Out-Host
 Get-Command -Module pester -Verbose | Select-Object -Property name, version -First 3 | Out-Host
 
 # Install https://github.com/nohwnd/Assert
-Install-Module -Name Assert -Force -ErrorAction Stop
+Install-Module -Name Assert -Force -MinimumVersion 0.9.5 -ErrorAction Stop
+Get-Module -Name Assert | Remove-Module
 Import-Module Assert -ErrorAction Stop
 
 Get-Module -Name Assert -Verbose | Out-Host
