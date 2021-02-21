dd69f1388cfa9c205fc5f27b94a9de1640c65ca9
Mikael Kalms <mikael@kalms.org>
2021-02-21 09:18:30 +0100

    Added more debug printing around Pester upgrade

--- windows/builder-workspace/InstallTestDependencies.ps1
+++ windows/builder-workspace/InstallTestDependencies.ps1
@@ -1,6 +1,12 @@
 # Install Pester (latest version, v5.0.0 or newer)
+Get-Module -Name Pester
+Get-Command -Module pester | Select-Object -Property name, version -First 3
 Install-Module -Name Pester -Force -SkipPublisherCheck -ErrorAction Stop
+Get-Module -Name Pester
+Get-Command -Module pester | Select-Object -Property name, version -First 3
 Import-Module Pester -ErrorAction Stop
+Get-Module -Name Pester
+Get-Command -Module pester | Select-Object -Property name, version -First 3
 
 # Install https://github.com/nohwnd/Assert
 Install-Module -Name Assert -Force -ErrorAction Stop
