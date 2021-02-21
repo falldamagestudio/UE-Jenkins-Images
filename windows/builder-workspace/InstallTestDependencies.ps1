553d647361cdb02455577fcff873cbc137d01fcc
Mikael Kalms <mikael@kalms.org>
2021-02-21 09:38:47 +0100

    Pipe debug prints to out-host

--- windows/builder-workspace/InstallTestDependencies.ps1
+++ windows/builder-workspace/InstallTestDependencies.ps1
@@ -1,12 +1,12 @@
 # Install Pester (latest version, v5.0.0 or newer)
-Get-Module -Name Pester
-Get-Command -Module pester | Select-Object -Property name, version -First 3
+Get-Module -Name Pester | Out-Host
+Get-Command -Module pester | Select-Object -Property name, version -First 3 | Out-Host
 Install-Module -Name Pester -Force -SkipPublisherCheck -ErrorAction Stop
-Get-Module -Name Pester
-Get-Command -Module pester | Select-Object -Property name, version -First 3
+Get-Module -Name Pester | Out-Host
+Get-Command -Module pester | Select-Object -Property name, version -First 3 | Out-Host
 Import-Module Pester -ErrorAction Stop
-Get-Module -Name Pester
-Get-Command -Module pester | Select-Object -Property name, version -First 3
+Get-Module -Name Pester | Out-Host
+Get-Command -Module pester | Select-Object -Property name, version -First 3 | Out-Host
 
 # Install https://github.com/nohwnd/Assert
 Install-Module -Name Assert -Force -ErrorAction Stop
