fe71e64692170b5f49760b30e536a6f1f240fbe1
Mikael Kalms <mikael@kalms.org>
2021-02-21 16:59:04 +0100

    Use #Requires to validate imported module versions

--- windows/builder-workspace/Ensure-TestToolVersions.ps1
+++ windows/builder-workspace/Ensure-TestToolVersions.ps1
@@ -1,11 +1,5 @@
-$MinimumPesterVersion = "5.0.0"
+# Pester: Please see https://pester.dev/docs/introduction/installation for installation instructions.
+#Requires -Modules @{ ModuleName="Pester"; ModuleVersion="5.0.0" }
 
-if (!(Get-Module -Name Pester -ListAvailable) -Or ((Get-Module -Name Pester -ListAvailable)[0].Version -lt [version]${MinimumPesterVersion})) {
-	throw "Pester ${MinimumPesterVersion} or later is required. Please see https://pester.dev/docs/introduction/installation for installation instructions."
-}
-
-$MinimumAssertVersion = "0.9.5"
-
-if (!(Get-Module -Name Assert -ListAvailable) -Or ((Get-Module -Name Assert -ListAvailable)[0].Version -lt [version]${MinimumAssertVersion})) {
-	throw "Assert ${MinimumAssertVersion} or later is required. Please see https://github.com/nohwnd/Assert for installation instructions."
-}
+# Assert: Please see https://github.com/nohwnd/Assert for installation instructions.
+#Requires -Modules @{ ModuleName="Assert"; ModuleVersion="0.9.5" }
