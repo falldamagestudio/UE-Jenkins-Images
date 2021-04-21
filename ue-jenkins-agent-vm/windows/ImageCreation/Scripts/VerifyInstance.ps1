e105dea9145659ff57167f472c293e337d20d4e5
Mikael Kalms <mikael@kalms.org>
2021-04-21 23:27:10 +0200

    Rolled Pester syntax for VerifyInstance back to pre-v5
    The script will get invoked on a native Win2019 machine.
    It has Pester pre-v5 installed by default.

--- ue-jenkins-agent-vm/windows/ImageCreation/Scripts/VerifyInstance.ps1
+++ ue-jenkins-agent-vm/windows/ImageCreation/Scripts/VerifyInstance.ps1
@@ -1,14 +1,13 @@
-. ${PSScriptRoot}\..\Tools\Scripts\Ensure-TestToolVersions.ps1
 
 Describe 'VerifyInstance' {
 
 	It "Has Win32 Long Paths enabled" {
 		$LongPathsEnabled = (Get-ItemProperty -path HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem -name "LongPathsEnabled").LongPathsEnabled
-		$LongPathsEnabled | Should -Not -Be 0
+		$LongPathsEnabled | Should Not Be 0
 	}
 
 	It "Has Jenkins Agent registered as a service" {
 		$Service = Get-Service "JenkinsAgent"
-		$Service | Should -Not -Be $null
+		$Service | Should Not Be $null
 	}
 }
