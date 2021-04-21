e23ae3c6a8da928252a5e1beea769dcf90d7138e
Mikael Kalms <mikael@kalms.org>
2021-04-21 23:27:36 +0200

    Explicit failures, cleaner output

--- ue-jenkins-agent-vm/windows/ImageCreation/Scripts/InstallSoftware.ps1
+++ ue-jenkins-agent-vm/windows/ImageCreation/Scripts/InstallSoftware.ps1
@@ -7,12 +7,12 @@ Enable-Win32LongPaths
 
 Write-Host "Creating folders for Jenkins..."
 
-New-Item -ItemType Directory -Path C:\J
-New-Item -ItemType Directory -Path C:\W
+New-Item -ItemType Directory -Path C:\J -ErrorAction Stop | Out-Null
+New-Item -ItemType Directory -Path C:\W -ErrorAction Stop | Out-Null
 
 Write-Host "Adding Windows Defender exclusion rule for Jenkins-related folders..."
 
-Add-WindowsDefenderExclusionRule -Folder C:\J
-Add-WindowsDefenderExclusionRule -Folder C:\W
+Add-WindowsDefenderExclusionRule -Folder C:\J -ErrorAction Stop
+Add-WindowsDefenderExclusionRule -Folder C:\W -ErrorAction Stop
 
 Write-Host "Done."
