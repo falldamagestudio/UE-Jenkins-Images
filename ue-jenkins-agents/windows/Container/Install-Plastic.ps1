95ed8dc793e49df50c8dc0e553929af10adff99b
Mikael Kalms <mikael@kalms.org>
2021-08-16 10:09:44 +0200

    Install Plastic client-only version for Windows

--- ue-jenkins-agents/windows/Container/Install-Plastic.ps1
+++ ue-jenkins-agents/windows/Container/Install-Plastic.ps1
@@ -4,12 +4,6 @@ class PlasticInstallerException : Exception {
 	PlasticInstallerException([int] $exitCode) : base("Plastic SCM installer exited with code ${exitCode}") { $this.ExitCode = $exitCode }
 }
 
-class PlasticConfigureServerException : Exception {
-	$ExitCode
-
-	PlasticConfigureServerException([int] $exitCode) : base("Plastic server configuration exited with code ${exitCode}") { $this.ExitCode = $exitCode }
-}
-
 function Install-Plastic {
 
 	$TempFolder = "C:\Temp"
@@ -23,21 +17,14 @@ function Install-Plastic {
 		# Determine installer download location
 		$InstallerLocation = (Join-Path -Path $TempFolder -ChildPath $InstallerExeName -ErrorAction Stop)
 
-		# Download Plastic SCM Cloud Installer
-		Invoke-WebRequest -UseBasicParsing -Uri "https://www.plasticscm.com/download/downloadinstaller/9.0.16.5201/plasticscm/windows/cloudedition" -OutFile $InstallerLocation -ErrorAction Stop
+		# Download Plastic SCM Client Installer
+		Invoke-WebRequest -UseBasicParsing -Uri "https://www.plasticscm.com/download/downloadinstaller/10.0.16.5882/plasticscm/windows/client" -OutFile $InstallerLocation -ErrorAction Stop
 
 		# Run installer
 		$Process = Start-Process -FilePath $InstallerLocation -ArgumentList "--mode","unattended" -NoNewWindow -Wait -PassThru
 	
 		if ($Process.ExitCode -ne 0) {
 			throw [PlasticInstallerException]::new($Process.ExitCode)
-		}
-
-		# Configure server
-		$Process2 = Start-Process -FilePath "C:\Program Files\PlasticSCM5\server\plasticd.exe" -ArgumentList "configure","--language=en","--workingmode=NameWorkingMode","--port=8084" -NoNewWindow -Wait -PassThru
-
-		if ($Process2.ExitCode -ne 0) {
-			throw [PlasticConfigureServerException]::new($Process2.ExitCode)
 		}
 
 	} finally {
