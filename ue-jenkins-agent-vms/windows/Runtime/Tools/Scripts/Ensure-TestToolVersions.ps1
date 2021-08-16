aa8a1d8500e2629f52987bd6b69e3a6ee28ed6a5
Mikael Kalms <mikael@kalms.org>
2021-08-16 15:57:10 +0200

    Reorganized powershell scripts

--- ue-jenkins-agent-vms/windows/Runtime/Tools/Scripts/Ensure-TestToolVersions.ps1
+++
@@ -1,5 +0,0 @@
-# Pester: Please see https://pester.dev/docs/introduction/installation for installation instructions.
-#Requires -Modules @{ ModuleName="Pester"; ModuleVersion="5.0.0" }
-
-# Assert: Please see https://github.com/nohwnd/Assert for installation instructions.
-#Requires -Modules @{ ModuleName="Assert"; ModuleVersion="0.9.5" }
--- ue-jenkins-buildtools/windows/Container/Enable-Win32LongPaths.ps1
+++
@@ -1,5 +0,0 @@
-function Enable-Win32LongPaths {
-
-	Set-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem -Name "LongPathsEnabled" -Value 1
-
-}
