2be920f4af3bcca86ceb8f390a7da7546b32b125
Mikael Kalms <mikael@kalms.org>
2021-08-17 21:59:43 +0200

    Convert to function

---
+++ Scripts/Windows/ImageBuilder/Host/Setup-DockerRegistryAuthentication.ps1
@@ -0,0 +1,27 @@
+function Setup-DockerRegistryAuthentication {
+	param (
+		[Parameter(Mandatory=$true)][string]$GceRegion,
+		[Parameter(Mandatory=$true)][string]$AgentKeyFile
+	)
+
+	$DockerCredentialGcrDownloadURL = "https://github.com/GoogleCloudPlatform/docker-credential-gcr/releases/download/v2.0.2/docker-credential-gcr_windows_amd64-2.0.2.tar.gz"
+
+	# Download docker-credential-gcr
+
+	Invoke-WebRequest -UseBasicParsing -Uri ${DockerCredentialGcrDownloadURL} -OutFile docker-credential-gcr.tgz -ErrorAction Stop
+	& tar -xzvf docker-credential-gcr.tgz
+
+	# Configure docker to use docker-credential gcr for authentication
+
+	& .\docker-credential-gcr.exe configure-docker --registries ${GceRegion}-docker.pkg.dev
+
+	# Use service account file for authentication
+
+	$GCloudConfigFolder = "${env:APPDATA}\gcloud"
+
+	if (!(Test-Path $GCloudConfigFolder)) {
+		New-Item -Type Directory $GCloudConfigFolder -ErrorAction Stop | Out-Null
+	}
+
+	Copy-Item -Path $AgentKeyFile -Destination "${GCloudConfigFolder}\application_default_credentials.json" -ErrorAction Stop
+}
