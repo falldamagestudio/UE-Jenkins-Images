92fd1049ee1dee1a3c0e3c9908e4587dea16562b
Mikael Kalms <mikael@kalms.org>
2021-08-19 23:06:14 +0200

    Fixed tests for Install-JenkinsRemotingAgent

--- Scripts/Windows/Applications/Install-JenkinsRemotingAgent.Tests.ps1
+++ Scripts/Windows/Applications/Install-JenkinsRemotingAgent.Tests.ps1
@@ -11,68 +11,70 @@ Describe 'Install-JenkinsRemotingAgent' {
 	It "Reports error if downloading agent jar fails" {
 
 		Mock Invoke-WebRequest { throw "Invoke-WebRequest failed" }
+		Mock Invoke-RestMethod { throw "Invoke-RestMethod should not be called" }
 		Mock Get-FileHash { throw "Get-FileHash should not be called" }
 
 		{ Install-JenkinsRemotingAgent -Path "C:\Jenkins" } |
 			Should -Throw "Invoke-WebRequest failed"
 
 		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 0 Invoke-RestMethod
 		Assert-MockCalled -Exactly -Times 0 Get-FileHash
 	}
 
 	It "Reports error if downloading sha1 hash fails" {
 
-		Mock Invoke-WebRequest -ParameterFilter { $Uri.AbsoluteUri.EndsWith(".jar") } { }
-		Mock Invoke-WebRequest -ParameterFilter { $Uri.AbsoluteUri.EndsWith("jar.sha1") } { throw "Invoke-WebRequest for sha1 hash failed" }
-		Mock Invoke-WebRequest { throw "Invalid invocation of Invoke-WebRequest" }
+		Mock Invoke-WebRequest { }
+		Mock Invoke-RestMethod { throw "Invoke-RestMethod for sha1 hash failed" }
 		Mock Get-FileHash { throw "Get-FileHash should not be called" }
 
 		{ Install-JenkinsRemotingAgent -Path "C:\Jenkins" } |
-			Should -Throw "Invoke-WebRequest for sha1 hash failed"
+			Should -Throw "Invoke-RestMethod for sha1 hash failed"
 
-		Assert-MockCalled -Exactly -Times 2 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 1 Invoke-RestMethod
 		Assert-MockCalled -Exactly -Times 0 Get-FileHash
 	}
 
 	It "Reports error if computing file hash fails" {
 
-		Mock Invoke-WebRequest -ParameterFilter { $Uri.AbsoluteUri.EndsWith(".jar") } { }
-		Mock Invoke-WebRequest -ParameterFilter { $Uri.AbsoluteUri.EndsWith("jar.sha1") } { @{ Content = "hash 1" } }
-		Mock Invoke-WebRequest { throw "Invalid invocation of Invoke-WebRequest" }
+		Mock Invoke-WebRequest { }
+		Mock Invoke-RestMethod { "hash 1" }
 		Mock Get-FileHash { throw "Get-FileHash failed" }
 
 		{ Install-JenkinsRemotingAgent -Path "C:\Jenkins" } |
 			Should -Throw "Get-FileHash failed"
 
-		Assert-MockCalled -Exactly -Times 2 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 1 Invoke-RestMethod
 		Assert-MockCalled -Exactly -Times 1 Get-FileHash
 	}
 
 	It "Reports error if hashes do not match" {
 
-		Mock Invoke-WebRequest -ParameterFilter { $Uri.AbsoluteUri.EndsWith(".jar") } { }
-		Mock Invoke-WebRequest -ParameterFilter { $Uri.AbsoluteUri.EndsWith("jar.sha1") } { @{ Content = "hash 1" } }
-		Mock Invoke-WebRequest { throw "Invalid invocation of Invoke-WebRequest" }
-		Mock Get-FileHash { return @{ Hash = "hassh 2"} }
+		Mock Invoke-WebRequest { }
+		Mock Invoke-RestMethod { "hash 1" }
+		Mock Get-FileHash { return @{ Hash = "hash 2"} }
 
 		{ Install-JenkinsRemotingAgent -Path "C:\Jenkins" } |
 			Should -Throw
 
-		Assert-MockCalled -Exactly -Times 2 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 1 Invoke-RestMethod
 		Assert-MockCalled -Exactly -Times 1 Get-FileHash
 	}
 
 	It "Reports success if hashes match" {
 
-		Mock Invoke-WebRequest -ParameterFilter { $Uri.AbsoluteUri.EndsWith(".jar") } { }
-		Mock Invoke-WebRequest -ParameterFilter { $Uri.AbsoluteUri.EndsWith("jar.sha1") } { @{ Content = "hash 1" } }
-		Mock Invoke-WebRequest { throw "Invalid invocation of Invoke-WebRequest" }
+		Mock Invoke-WebRequest { }
+		Mock Invoke-RestMethod { "hash 1" }
 		Mock Get-FileHash { return @{ Hash = "hash 1"} }
 
 		{ Install-JenkinsRemotingAgent -Path "C:\Jenkins" } |
 			Should -Not -Throw
 
-		Assert-MockCalled -Exactly -Times 2 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
+		Assert-MockCalled -Exactly -Times 1 Invoke-RestMethod
 		Assert-MockCalled -Exactly -Times 1 Get-FileHash
 	}
 }
