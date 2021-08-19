f3776c6e6099121edd0c76d011597d482c9441c7
Mikael Kalms <mikael@kalms.org>
2021-08-19 23:02:54 +0200

    swarm-client is downloaded from the web again, found a good url

--- Scripts/Windows/Applications/Install-JenkinsSwarmAgent.Tests.ps1
+++ Scripts/Windows/Applications/Install-JenkinsSwarmAgent.Tests.ps1
@@ -8,25 +8,23 @@ BeforeAll {
 
 Describe 'Install-JenkinsSwarmAgent' {
 
-# HACK: Comment out while we are copying this from local location instead of downloading from the internet
-#
-#	It "Reports error if downloading agent jar fails" {
-#
-#		Mock Invoke-WebRequest { throw "Invoke-WebRequest failed" }
-#
-#		{ Install-JenkinsSwarmAgent -Path "C:\Jenkins" } |
-#			Should -Throw "Invoke-WebRequest failed"
-#
-#		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
-#	}
-#
-#	It "Reports success if download is successful" {
-#
-#		Mock Invoke-WebRequest { }
-#
-#		{ Install-JenkinsSwarmAgent -Path "C:\Jenkins" } |
-#			Should -Not -Throw
-#
-#		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
-#	}
+	It "Reports error if downloading agent jar fails" {
+
+		Mock Invoke-WebRequest { throw "Invoke-WebRequest failed" }
+
+		{ Install-JenkinsSwarmAgent -Path "C:\Jenkins" } |
+			Should -Throw "Invoke-WebRequest failed"
+
+		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
+	}
+
+	It "Reports success if download is successful" {
+
+		Mock Invoke-WebRequest { }
+
+		{ Install-JenkinsSwarmAgent -Path "C:\Jenkins" } |
+			Should -Not -Throw
+
+		Assert-MockCalled -Exactly -Times 1 Invoke-WebRequest
+	}
 }
--- Scripts/Windows/Applications/Install-JenkinsSwarmAgent.ps1
+++ Scripts/Windows/Applications/Install-JenkinsSwarmAgent.ps1
@@ -4,12 +4,13 @@ function Install-JenkinsSwarmAgent {
 		[Parameter(Mandatory)] [string] $Path
     )
 
-    $DownloadUrl = "https://repo.jenkins-ci.org/native/releases/org/jenkins-ci/plugins/swarm-client/3.25/swarm-client-3.25.jar"
+    # The official download URL is on the format,
+    # https://repo.jenkins-ci.org/native/releases/org/jenkins-ci/plugins/swarm-client/<version>/swarm-client-<version>.jar
+    # but when we hit that directly we get 404 back
+    # Therefore we use an artifactory-related URL instead - it appears to work better
+    $DownloadUrl = "https://repo.jenkins-ci.org/artifactory/releases/org/jenkins-ci/plugins/swarm-client/3.25/swarm-client-3.25.jar"
     $TargetFile = "${Path}\swarm-agent.jar"
 
-    # Download Jenkins Swarm agent jar, and place it in a default location
-    #Invoke-WebRequest -UseBasicParsing -Uri $DownloadUrl -OutFile $TargetFile
-    #
-    # HACK: include the jar within the repo, since the download URL seems to return 404 when being hit it directly
-    Copy-Item -Path "${PSScriptRoot}\..\..\..\VMs\stuff\swarm-client-3.25.jar" -Destination $TargetFile
+    #Download Jenkins Swarm agent jar, and place it in a default location
+    Invoke-WebRequest -UseBasicParsing -Uri $DownloadUrl -OutFile $TargetFile
 }
