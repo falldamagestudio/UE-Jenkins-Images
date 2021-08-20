06d44823535d5c928d5e321e8c80087be86bd8f3
Mikael Kalms <mikael@kalms.org>
2021-08-20 09:31:33 +0200

    Fixed include path

--- Docker/buildtools/windows/HostBuild.ps1
+++ Docker/buildtools/windows/HostBuild.ps1
@@ -12,7 +12,7 @@ param (
 . ${PSScriptRoot}\..\..\..\Scripts\Windows\ImageBuilder\Host\Build-DockerImage.ps1
 . ${PSScriptRoot}\..\..\..\Scripts\Windows\ImageBuilder\Host\Push-DockerImage.ps1
 . ${PSScriptRoot}\..\..\..\Scripts\Windows\ImageBuilder\Host\Setup-DockerRegistryAuthentication.ps1
-. ${PSScriptRoot}\..\..\..\Scripts\Windows\BuildSteps\BuildStep-InstallBuildTools-Container.ps1
+. ${PSScriptRoot}\..\..\..\Scripts\Windows\BuildSteps\BuildStep-InstallBuildTools-Host.ps1
 . ${PSScriptRoot}\..\..\..\Scripts\Windows\ImageBuilder\Copy-SystemDLLs.ps1
 
 Write-Host "Resizing C: partition to use all available disk space..."
