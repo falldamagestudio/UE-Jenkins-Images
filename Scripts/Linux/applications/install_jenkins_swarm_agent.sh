bf08714f58100f715e34bc83e6bcf17a288438f3
Mikael Kalms <mikael@kalms.org>
2021-08-28 14:11:53 +0200

    Fetch swarm agent jar

---
+++ Scripts/Linux/applications/install_jenkins_swarm_agent.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+function install_jenkins_swarm_agent () {
+
+    DOWNLOAD_URL="https://repo.jenkins-ci.org/artifactory/releases/org/jenkins-ci/plugins/swarm-client/3.25/swarm-client-3.25.jar"
+
+    TARGET_FILE="${HOME}/swarm-agent.jar"
+
+    wget --progress=dot:giga -O "$TARGET_FILE" "$DOWNLOAD_URL"
+}
--- VMs/agents/linux/swarm-agent/install.sh
+++ VMs/agents/linux/swarm-agent/install.sh
@@ -8,7 +8,9 @@ shopt -s inherit_errexit
 . "${BASH_SOURCE%/*}/../../../../Scripts/Linux/buildsteps/buildstep_install_buildtools.sh"
 . "${BASH_SOURCE%/*}/../../../../Scripts/Linux/buildsteps/buildstep_install_scm_tools.sh"
 . "${BASH_SOURCE%/*}/../../../../Scripts/Linux/applications/install_adoptium_openjdk.sh"
+. "${BASH_SOURCE%/*}/../../../../Scripts/Linux/applications/install_jenkins_swarm_agent.sh"
 
 buildstep_install_buildtools || exit 1
 buildstep_install_scm_tools || exit 1
 install_adoptium_openjdk || exit 1
+install_jenkins_swarm_agent || exit 1
