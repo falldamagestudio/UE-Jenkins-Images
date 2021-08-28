dfcb41780b43a7569add1b8c9282e20b286ecab9
Mikael Kalms <mikael@kalms.org>
2021-08-28 21:54:15 +0200

    Install Adoptium OpenJDK for all users

--- Scripts/Linux/applications/install_adoptium_openjdk.sh
+++ Scripts/Linux/applications/install_adoptium_openjdk.sh
@@ -4,20 +4,23 @@ function install_adoptium_openjdk () {
 
     local DOWNLOAD_URL="https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.12%2B7/OpenJDK11U-jdk_x64_linux_hotspot_11.0.12_7.tar.gz"
 
-    local ARCHIVE_FILE=~/adoptium_openjdk.tar.gz
-    local INSTALL_ROOT_DIR=~
+    # Temp archive location
+    local ARCHIVE_FILE="${HOME}/adoptium_openjdk.tar.gz"
 
-    wget --progress=dot:giga -O "${ARCHIVE_FILE}" "$DOWNLOAD_URL" 2>&1 || return
+    # JDK will be placed in a folder named ${INSTALL_ROOT_DIR}/jdk-<version>
+    local INSTALL_ROOT_DIR=/usr/lib/jvm
 
-    tar --directory "${INSTALL_ROOT_DIR}" -xzf "${ARCHIVE_FILE}" || return
+    # Download and unpack JDK
+
+    wget --progress=dot:giga -O "${ARCHIVE_FILE}" "${DOWNLOAD_URL}" 2>&1 || return
+    sudo mkdir -p "${INSTALL_ROOT_DIR}" || return
+    sudo tar --directory "${INSTALL_ROOT_DIR}" -xzf "${ARCHIVE_FILE}" || return
+    rm -r "${ARCHIVE_FILE}" || return
+
+    # Create symlink for java binary
 
     local JDK_ROOT_DIR
     JDK_ROOT_DIR=$(realpath "$(find "${INSTALL_ROOT_DIR}" -maxdepth 1 -name "jdk-*" -type d)") || return
     local JDK_BIN_DIR="${JDK_ROOT_DIR}/bin"
-
-    export PATH=${JDK_BIN_DIR}:${PATH}
-
-    echo "export PATH=\"${JDK_BIN_DIR}:${PATH}\"" >> ~/.profile || return
-
-    rm -r "${ARCHIVE_FILE}"
+    sudo ln -s "${JDK_BIN_DIR}/java" /usr/bin/java || return
 }
