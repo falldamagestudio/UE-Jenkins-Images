80e08c0c0566f22ec03e129b7d073065606625cc
Mikael Kalms <mikael@kalms.org>
2021-08-29 10:15:13 +0200

    Renamed vm startup service

--- Scripts/Linux/buildsteps/buildstep_install_services.sh
+++ Scripts/Linux/buildsteps/buildstep_install_services.sh
@@ -6,7 +6,7 @@ function buildstep_install_services () {
 
     JENKINS_AGENT_UNIT_FILE=$1
 
-    install_service "${BASH_SOURCE%/*}/../agents/services/vm_startup.service" "vm_startup.service"
+    install_service "${BASH_SOURCE%/*}/../agents/services/gce_service_vm_startup.service" "vm_startup.service"
 
     if [ "${JENKINS_AGENT_UNIT_FILE}" != "" ]; then
         install_service "${JENKINS_AGENT_UNIT_FILE}" "jenkins_agent.service"
