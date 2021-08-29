dd4c7716f82647aafbb48cf9f04acf8f4aef77e1
Mikael Kalms <mikael@kalms.org>
2021-08-29 22:52:46 +0200

    Enable service as part of installing it

--- Scripts/Linux/system_configuration/install_service.sh
+++ Scripts/Linux/system_configuration/install_service.sh
@@ -3,7 +3,11 @@
 function install_service () {
 
     SOURCE_SYSTEMD_UNIT_FILE=$1
-    TARGET_FILENAME=$2
+    SERVICE_NAME=$2
 
-    sudo cp "${SOURCE_SYSTEMD_UNIT_FILE}" "/etc/systemd/system/${TARGET_FILENAME}"
+    sudo cp "${SOURCE_SYSTEMD_UNIT_FILE}" "/etc/systemd/system/${SERVICE_NAME}" || return
+
+    sudo systemctl daemon-reload || return
+
+    sudo systemctl enable "${SERVICE_NAME}" || return
 }
