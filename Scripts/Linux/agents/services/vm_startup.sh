283a75bec3cf17f5c1ab6693a1d6e98d04f33393
Mikael Kalms <mikael@kalms.org>
2021-08-29 09:39:24 +0200

    Muted shellcheck warnings

--- Scripts/Linux/agents/services/vm_startup.sh
+++ Scripts/Linux/agents/services/vm_startup.sh
@@ -7,8 +7,11 @@ echo "VM Startup script starting..."
 
 echo "Fetching Plastic SCM client config from Secrets Manager..."
 
+#shellcheck disable=SC2034 # shellcheck doesn't understand that KEYS is being passed by reference to get_gce_settings
 KEYS=("plastic-config-tgz")
+#shellcheck disable=SC2034 # shellcheck doesn't understand that SOURCES is being passed by reference to get_gce_settings
 SOURCES=("secret")
+#shellcheck disable=SC2034 # shellcheck doesn't understand that BINARY is being passed by reference to get_gce_settings
 BINARY=("true")
 RESULT=()
 get_gce_settings KEYS SOURCES BINARY RESULT "false" "true"
