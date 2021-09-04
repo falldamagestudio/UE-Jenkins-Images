68d8bec0c43f22f94f1d73c13ac99235e4afa4e5
Mikael Kalms <mikael@kalms.org>
2021-09-04 16:06:34 +0200

    Windows VM downloading Swarm agent bugfixed

--- Scripts/Windows/Applications/Install-JenkinsSwarmAgent.ps1
+++ Scripts/Windows/Applications/Install-JenkinsSwarmAgent.ps1
@@ -8,6 +8,9 @@ function Install-JenkinsSwarmAgent {
 
     $TargetFile = "${Path}\swarm-agent.jar"
 
-    #Download Jenkins Swarm agent jar, and place it in a default location
-    Invoke-WebRequest -UseBasicParsing -Uri $ToolsAndVersions.JenkinsSwarmAgentDownloadUrl -OutFile $TargetFile -ErrorAction Stop
+    # Download Jenkins Swarm agent jar, and place it in a default location
+    # We specify a blank UserAgent, because the default user agent makes JFrog's repository server
+    #  assume that this is an interactive browser - and therefore, it checks for JavaScript support
+    #  - and fails us because of lack thereof
+    Invoke-WebRequest -UseBasicParsing -UserAgent "" -Uri $ToolsAndVersions.JenkinsSwarmAgentDownloadUrl -OutFile $TargetFile -ErrorAction Stop
 }
