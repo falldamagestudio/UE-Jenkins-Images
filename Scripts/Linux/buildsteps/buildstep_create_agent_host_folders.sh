b6c14e278a3d46b59a470c391b2e04e52cb9bbd1
Mikael Kalms <mikael@kalms.org>
2021-09-04 15:45:27 +0200

    Create remoting folder on linux vms

--- Scripts/Linux/buildsteps/buildstep_create_agent_host_folders.sh
+++ Scripts/Linux/buildsteps/buildstep_create_agent_host_folders.sh
@@ -3,10 +3,15 @@
 function buildstep_create_agent_host_folders () {
 
     JENKINS_AGENT_FOLDER=${HOME}/agent
+    JENKINS_REMOTING_FOLDER=${HOME}/agent/remoting
     JENKINS_WORKSPACE_FOLDER=${HOME}/workspace
 
     echo "Creating folders for Jenkins..."
 
     mkdir -p "${JENKINS_AGENT_FOLDER}"
+
+    # The Swarm agent requires that the remoting folder exists
+    mkdir -p "${JENKINS_REMOTING_FOLDER}"
+
     mkdir -p "${JENKINS_WORKSPACE_FOLDER}"
 }
