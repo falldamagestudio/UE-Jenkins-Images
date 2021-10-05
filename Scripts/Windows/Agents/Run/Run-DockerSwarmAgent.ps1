85ce93ede96287b14b123b60589b23ef4dffe469
Mikael Kalms <mikael@kalms.org>
2021-10-05 10:36:39 +0200

    Added extra comments + ensured that Run-SwarmAgent runs in exclusive mode

--- Scripts/Windows/Agents/Run/Run-DockerSwarmAgent.ps1
+++ Scripts/Windows/Agents/Run/Run-DockerSwarmAgent.ps1
@@ -21,6 +21,9 @@ function Run-DockerSwarmAgent {
 	)
 
 	$Arguments = @(
+
+		# Docker run parameters
+
 		"--rm"
 		"--name","jenkins-agent"
 		# Share Jenkins agent work folder with containers
@@ -34,18 +37,29 @@ function Run-DockerSwarmAgent {
 		# Enable docker CLI users inside containers to communicate with Docker daemon
 		"-v","\\.\pipe\docker_engine:\\.\pipe\docker_engine"
 		$AgentImageUrl
+
+		# Swarm client settings; reference: https://plugins.jenkins.io/swarm/#documentation
+
 		# Use Websocket protocol
 		"-webSocket"
 		"-executors","${NumExecutors}"
 		"-labels",$Labels
-		# Only build jobs with label expressions matching this node
+		# Only build jobs with label expressions matching this node's labels
 		"-mode","exclusive"
 		"-master",$JenkinsUrl
 		"-workDir",$JenkinsAgentFolder
 		"-username",$AgentUsername
 		"-password",$AgentAPIToken
+		# Do not add a random suffix to the node name
 		"-disableClientsUniqueId"
+		# When going online, if the node already exists, then delete the previous node
+		# In some scenarios, a failed client startup results in the Swarm plugin creating the node, but not
+		#  deleting it afterward. With this setting we are ensured that subsequent starts of the same
+		#  client will not fail (because the node already exists).
+		# On the other hand, this will cause weird situations if multiple clients are started with the
+		#  same exact name. That is a smaller problem, so we choose to have this flag present
 		"-deleteExistingClients"
+		# Fail if ${JenkinsAgentFolder} is missing
 		"-failIfWorkDirIsMissing"
 		"-name",$AgentName
 
--- Scripts/Windows/Agents/Run/Run-SwarmAgent.ps1
+++ Scripts/Windows/Agents/Run/Run-SwarmAgent.ps1
@@ -21,18 +21,35 @@ function Run-SwarmAgent {
 	$AgentJarLocation = "${JenkinsAgentFolder}\swarm-agent.jar"
 
 	$Arguments = @(
+
+		# Java parameters
+
 		"-jar"
 		$AgentJarLocation
+
+		# Swarm client settings; reference: https://plugins.jenkins.io/swarm/#documentation
+
 		"-master",$JenkinsUrl
 		"-workDir",$JenkinsAgentFolder
 		"-username",$AgentUsername
 		"-password",$AgentAPIToken
 		"-executors",$NumExecutors
 		"-labels","""${Labels}"""
+		# Use Websocket protocol
 		"-webSocket"
+		# Only build jobs with label expressions matching this node's labels
+		"-mode","exclusive"
+		# Do not add a random suffix to the node name
 		"-disableClientsUniqueId"
-		"-failIfWorkDirIsMissing"
+		# When going online, if the node already exists, then delete the previous node
+		# In some scenarios, a failed client startup results in the Swarm plugin creating the node, but not
+		#  deleting it afterward. With this setting we are ensured that subsequent starts of the same
+		#  client will not fail (because the node already exists).
+		# On the other hand, this will cause weird situations if multiple clients are started with the
+		#  same exact name. That is a smaller problem, so we choose to have this flag present
 		"-deleteExistingClients"
+		# Fail if ${JenkinsAgentFolder} is missing
+		"-failIfWorkDirIsMissing"
 		"-name",$AgentName)
 
 	$JavaHome = $env:JAVA_HOME
