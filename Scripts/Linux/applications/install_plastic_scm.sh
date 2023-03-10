6086a1844361ded6bcd5cec7ddcac8606657a341
Mikael Kalms <mikael@kalms.org>
2023-03-10 15:47:17 +0100

    Migrated from apt-key to per-repo signing for plastic install step

--- Scripts/Linux/applications/install_plastic_scm.sh
+++ Scripts/Linux/applications/install_plastic_scm.sh
@@ -4,8 +4,8 @@ function install_plastic_scm () {
 
     ${SUDO} apt-get update || return
     ${SUDO} apt-get install -y apt-transport-https ca-certificates wget gnupg --no-install-recommends || return
-    { echo "deb https://www.plasticscm.com/plasticrepo/stable/debian/ ./" || return ; } | { ${SUDO} tee /etc/apt/sources.list.d/plasticscm-stable.list || return ; }
-    { wget https://www.plasticscm.com/plasticrepo/stable/debian/Release.key --quiet -O - || return ; } | { ${SUDO} apt-key add - || return ; }
+    { curl -fsSL https://www.plasticscm.com/plasticrepo/stable/debian/Release.key || return ; } | { ${SUDO} gpg --dearmor --yes -o /usr/share/keyrings/plasticscm.gpg || return ; }
+    { echo "deb [arch=amd64 signed-by=/usr/share/keyrings/plasticscm.gpg] https://www.plasticscm.com/plasticrepo/stable/debian/ ./" || return ; } | { ${SUDO} tee /etc/apt/sources.list.d/plasticscm-stable.list || return ; }
     ${SUDO} apt-get update || return
     ${SUDO} apt-get install -y plasticscm-client-core --no-install-recommends || return
 
